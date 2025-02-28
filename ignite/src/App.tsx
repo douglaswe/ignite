bdc8867f0ccd4e293b82965e00aa8a08eda77d5e
Douglas José <79637913+douglaswe@users.noreply.github.com>
2025-02-28 20:40:56 +0000

    initial project

---
+++ ignite/src/App.tsx
@@ -0,0 +1,16 @@
+import { Header } from "./components/Header";
+import { Post } from "./Post";
+import  "./global.css";
+
+
+function App() {
+
+  return (
+    <>
+    <Header/>
+    <Post author='Douglas' content='bla bla'/>    
+    </>
+  )
+}
+
+export default App
