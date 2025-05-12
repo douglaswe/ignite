61a40436744fd368e15a08042848b4a736f6fe15
Douglas José <79637913+douglaswe@users.noreply.github.com>
2025-05-12 19:58:03 +0000

    refatora componente Avatar para aceitar props genéricas

--- ignite/src/components/Comment.tsx
+++ ignite/src/components/Comment.tsx
@@ -26,13 +26,13 @@ export function Comment({ content, onDeleteComment }: CommentProps) {
 
     return (
         <div className={styles.comment}>
-            <Avatar hasBorder={false} src="https://github.com/douglaswe.png" alt="" />
+            <Avatar hasBorder={false} src="https://i.pravatar.cc/150?img=8" alt="Avatar do usuário" />
 
             <div className={styles.commentBox}>
                 <div className={styles.commentContent}>
                     <header>
                         <div className={styles.authorAndTime}>
-                            <strong>Diego Fernandes</strong>
+                            <strong>Diogo</strong>
                             <time title="11 de Maio às 08:13" dateTime="2023-05-11 08:13:30">
                                 Cerca de 1h atrás
                             </time>
